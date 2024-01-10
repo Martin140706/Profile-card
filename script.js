65c35496c8e43e13aba21a6b6ec6fa8012f8f1d5
Martin Tolosa <tolosamartintecnica@gmail.com>
2024-01-10 15:35:42 -0300

    Primer commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function Mensaje() {
+    window.open('https://wa.link/yr17h8');
+}
+function linkedin() {
+    window.open('https://www.linkedin.com/in/martin-tolosa-569695266/');
+}
+function instagram() {
+    window.open('https://www.instagram.com/_martinn.06_/');
+}
+function twitter() {
+    window.open('https://twitter.com/Martuu1407');
+}
+function github() {
+    window.open('https://github.com/Martin140706');
+}
